8bbca06740edecbbe71a7549df94c8dcb5db95a1
ugyenrabten <03230405.gcbs@rub.edu.bt>
2024-03-07 14:15:43 -0800

    stringEx

---
+++ strings EX.py
@@ -0,0 +1,7 @@
+# 1.get user input
+# convert the user input into all upper case
+# print the converted string
+
+user = input('Enter a string:')
+userconverted =  user.upper()
+print(userconverted)
