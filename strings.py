62437000a0cd4192cc2252ab45194bf54c555797
ugyenrabten <03230405.gcbs@rub.edu.bt>
2024-03-07 14:05:08 -0800

    strings

---
+++ strings.py
@@ -0,0 +1,38 @@
+# ! STRING MANIPULATION
+
+# ! string concatenation
+string1 = "2"
+string2 = "3"
+
+combined = string1 + string2
+combined1 = "hi" + " " + "there"
+
+print('String Concatenation')
+print(combined)
+print(combined1)
+
+# ! string interpolation
+print('String Interpolation')
+# ! how do you print "john is 30 years of age"
+name = "John"
+age = 99
+interpolated = f"John is {age} years of age"
+print(interpolated)
+
+# ! string methods
+print('String Methods')
+# # ! upper
+print('String upper')
+stringlower = 'hello how are you'
+converted =  stringlower.upper()
+converted1 = "pure string".upper()
+
+# ! lower
+print('String lower')
+lower = "HELLO".lower()
+print(lower)
+
+# # ! capitalize
+print('String capitalize')
+capitalize = "hello there".capitalize()
+print(capitalize)
