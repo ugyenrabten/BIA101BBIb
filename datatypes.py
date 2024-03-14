b3db97861e2d7913817d1b9536c818ac646e37da
ugyenrabten <03230405.gcbs@rub.edu.bt>
2024-03-14 14:18:21 -0800

    .

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+# creation of array
+array1 = [99,2,3,"Gay",25]
+print(array1)
+
+# retrieving
+element1 = array1[0]
+element2 = array1[3]
+lastElement = array1[-3]
+print(lastElement)
+
+# modifying elements
+array1[0] = 10
+print(array1)
+
+# getting number of elements in an array
+no_of_elements = len(array1)
